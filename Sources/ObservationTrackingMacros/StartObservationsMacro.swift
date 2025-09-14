79c6abe4af1eaccb0f83c25e971661c07ed58590
Volodymyr Andriienko <vandrjios@gmail.com>
2025-09-14 19:54:15 +0300

    [core] Split macros

---
+++ Sources/ObservationTrackingMacros/StartObservationsMacro.swift
@@ -0,0 +1,28 @@
+//
+//  StartObservationsMacro.swift
+//  ObservationTracking
+//
+//  Created by VAndrJ on 14.09.2025.
+//
+
+import SwiftSyntax
+import SwiftSyntaxMacros
+
+public struct StartObservationsMacro: BodyMacro {
+    public static func expansion(
+        of node: AttributeSyntax,
+        providingBodyFor declaration: some DeclSyntaxProtocol & WithOptionalCodeBlockSyntax,
+        in context: some MacroExpansionContext
+    ) throws -> [CodeBlockItemSyntax] {
+        guard let functionDecl = declaration.as(FunctionDeclSyntax.self),
+            let body = functionDecl.body
+        else {
+            throw MacroExpansionErrorMessage("@StartObservations can only be applied to functions with a body")
+        }
+
+        var newStatements = Array(body.statements)
+        newStatements.append("startObservationsIfNeeded()")
+
+        return newStatements
+    }
+}
---
+++ Sources/ObservationTrackingMacros/StopObservationsMacro.swift
@@ -0,0 +1,28 @@
+//
+//  StopObservationsMacro.swift
+//  ObservationTracking
+//
+//  Created by VAndrJ on 14.09.2025.
+//
+
+import SwiftSyntax
+import SwiftSyntaxMacros
+
+public struct StopObservationsMacro: BodyMacro {
+    public static func expansion(
+        of node: AttributeSyntax,
+        providingBodyFor declaration: some DeclSyntaxProtocol & WithOptionalCodeBlockSyntax,
+        in context: some MacroExpansionContext
+    ) throws -> [CodeBlockItemSyntax] {
+        guard let functionDecl = declaration.as(FunctionDeclSyntax.self),
+            let body = functionDecl.body
+        else {
+            throw MacroExpansionErrorMessage("@StopObservations can only be applied to functions with a body")
+        }
+
+        var newStatements = Array(body.statements)
+        newStatements.append("stopObservations()")
+
+        return newStatements
+    }
+}
